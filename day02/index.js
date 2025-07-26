2da9f4d057e3a3807620f8d6edeeb6953818a743
Rashed Mahmud <rashed.webdev@gmail.com>
2025-07-26 17:56:33 +0600

    day 02 task done

---
+++ day02/index.js
@@ -0,0 +1,17 @@
+const name = "John Doe";
+const age = 35;
+const isStudent = true;
+const favLanguage = "JavaScript";
+
+const jobDesc = {
+  title: "Web developer",
+  salary: 20000,
+  location: "Dhaka",
+  isRemote: true
+};
+
+const languageList = ["JavaScript", "Python", "Java", "C++"];
+
+console.log(name, age, isStudent, favLanguage);
+console.log(jobDesc);
+console.log(language);
